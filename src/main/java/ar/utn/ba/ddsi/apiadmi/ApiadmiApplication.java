c328674deed12f56a182853f6c5c2b75b77498e0
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2025-09-02 23:55:05 -0300

    .

---
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/ApiadmiApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.ba.ddsi.apiadmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiadmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiadmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/utn/ba/ddsi/apiadmi/ApiadmiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.ba.ddsi.apiadmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiadmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
