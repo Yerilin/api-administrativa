407e784bf50c386bcea71cb98d3bb484c5e4a6c9
Yerilin Villarreal <yvillarrealmamani@frba.utn.edu.ar>
2025-09-05 08:59:41 -0300

    Update hecho

--- src/main/java/ar/utn/ba/ddsi/apiadmi/models/entities/Hecho.java
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/models/entities/Hecho.java
@@ -1,4 +1,14 @@
 package ar.utn.ba.ddsi.apiadmi.models.entities;
 
+@Data
 public class Hecho {
+  private String Titulo;
+    private String descripcion;
+    private Categoria categoria;
+    private LocalDate fecha;
+    private LocalDate fechaDeCarga;
+    private List<Fuente> fuentes;
+    private Ubicacion lugarDeOcurrencia;
+    private Fuente fuenteOrigen;
+    private EnumEstado estado;
 }
