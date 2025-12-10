aa2c65389fb063ba1215b18511149626b798a7c9
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2025-12-09 23:06:05 -0300

    cambio la BBDD

---
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/controllers/CriteriosController.java
@@ -0,0 +1,28 @@
+package ar.utn.ba.ddsi.apiadmi.controllers;
+
+import ar.utn.ba.ddsi.apiadmi.models.dtos.ColeccionDto;
+import ar.utn.ba.ddsi.apiadmi.models.entities.hecho.Categoria;
+import ar.utn.ba.ddsi.apiadmi.models.repository.ICategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/criterios")
+@CrossOrigin(origins= "http://localhost:3000")
+public class CriteriosController {
+
+    @Autowired
+    private ICategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> obtenerColecciones(){
+
+        return this.categoriaRepository.findAll();
+
+    }
+}
