2ab454692647524b7519d19b302d04361013b618
Aponte Lucas <laponte@frba.utn.edu.ar>
2026-02-14 19:36:43 -0300

    monitoreoHecho

---
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/monitoring/MonitorAdminController.java
@@ -0,0 +1,35 @@
+package ar.utn.ba.ddsi.apiadmi.monitoring;
+
+import ar.utn.ba.ddsi.apiadmi.monitoring.healthindicators.DatabaseHealthIndicator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/monitor")
+public class MonitorAdminController {
+
+    private final DatabaseHealthIndicator database;
+
+    public MonitorAdminController(
+            DatabaseHealthIndicator database
+    ) {
+
+        this.database = database;
+    }
+
+    // ---------- FALLAS ----------
+
+    @PostMapping("/fail/database")
+    public void failDatabase() {
+        database.markDown();
+    }
+
+
+    // ---------- RECUPERACIÓN ----------
+
+    @PostMapping("/recover/all")
+    public void recoverAll() {
+        database.markUp();
+    }
+}
