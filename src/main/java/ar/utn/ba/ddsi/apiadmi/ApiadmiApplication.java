d5b8f71abe509af06f03fe8a95da1dd5e2bbb33b
Aponte Lucas <laponte@frba.utn.edu.ar>
2026-02-20 13:02:34 -0300

    agregoEnableSchedulinnng

--- src/main/java/ar/utn/ba/ddsi/apiadmi/ApiadmiApplication.java
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/ApiadmiApplication.java
@@ -3,8 +3,10 @@ package ar.utn.ba.ddsi.apiadmi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 
 public class ApiadmiApplication {
 
