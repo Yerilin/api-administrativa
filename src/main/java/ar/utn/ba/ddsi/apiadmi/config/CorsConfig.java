2abed247c1a2580872c8e57eb7c3db8a8aab444c
Aponte Lucas <laponte@frba.utn.edu.ar>
2026-02-21 05:32:45 -0300

    agregoCors

--- src/main/java/ar/utn/ba/ddsi/apiadmi/config/CorsConfig.java
+++ src/main/java/ar/utn/ba/ddsi/apiadmi/config/CorsConfig.java
@@ -26,7 +26,9 @@ public class CorsConfig {
                 "https://front-metamapa-lo3l.vercel.app",
                 "https://front-metamapa.vercel.app",
                 "http://localhost:3000",
-                "https://front-metamapa-dun.vercel.app"
+                "https://front-metamapa-dun.vercel.app",
+                "https://front-metamapa.vercel.app/"
+
         ));
 
         // 3. Cabeceras y métodos permitidos (OPTIONS es obligatorio)
